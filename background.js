806b891b013fa9a932f582dd08ea35ce9a16889b
xtreezzz <39224286+xtreezzz@users.noreply.github.com>
2025-06-29 17:31:46 +0300

    Add background service worker and fix rule listener

--- background.js
+++ background.js
@@ -1,8 +1,8 @@
-chrome.declarativeNetRequest.onRuleMatched.addListener((rule) => {
-  if (rule.rule.id === 1) {
-    fetch(rule.request.url)
-      .then(response => response.json())
-      .then(data => {
+chrome.declarativeNetRequest.onRuleMatchedDebug.addListener((info) => {
+  if (info.rule.ruleId === 1) {
+    fetch(info.request.url)
+      .then((response) => response.json())
+      .then((data) => {
         chrome.storage.local.set({ zeppelinData: data });
       });
   }
