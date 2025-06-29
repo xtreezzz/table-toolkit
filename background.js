86df3e25607d0cd15afe4e35aa6a94f165b61d7c
xtreezzz <39224286+xtreezzz@users.noreply.github.com>
2025-06-29 17:42:43 +0300

    Restore missing files and clean manifest

--- background.js
+++ background.js
@@ -1,9 +1,10 @@
-chrome.declarativeNetRequest.onRuleMatched.addListener((rule) => {
-  if (rule.rule.id === 1) {
-    fetch(rule.request.url)
-      .then(response => response.json())
-      .then(data => {
+chrome.declarativeNetRequest.onRuleMatchedDebug.addListener((info) => {
+  if (info.rule.ruleId === 1) {
+    fetch(info.request.url)
+      .then((response) => response.json())
+      .then((data) => {
         chrome.storage.local.set({ zeppelinData: data });
-      });
+      })
+      .catch(err => console.error('Fetch failed', err));
   }
 });
